ff8a28331714f0af3e523837f5f23d20ebbc95dc
david <dmsim159@gmail.com>
2025-06-24 21:11:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from pytube import YouTube
 import tkinter as tk
-from tkinter import fileddialog
+from tkinter import filedialog
 
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url) #grabs an instance of the YouTube video
+        streams = yt.streams.filter(progressive=True, file_extension='mp4')
+        highest_res_stream = streams.get_highest_resolution() #gets the highest resolution stream
+        highest_res_stream.download(output_path=save_path) #downloads the video to the specified path
+        print(f"Downloaded: {yt.title} to {save_path}")
+        
+    except Exception as e:
+        print(e)
+        
+url = ""
+save_path = "D:/videos"
+
+download_video(url, save_path)
+        
